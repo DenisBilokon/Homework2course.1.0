2cfbb0adea088d34f92fe626a62776fffcf3b62e
DenisBilokon <106659593+DenisBilokon@users.noreply.github.com>
2023-02-15 09:28:33 +0200

    Add files via upload

---
+++ Program2.1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2c2s.hw1._1
+{
+    internal class Program
+    {
+        public delegate int MyDel(int a, int b);
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Введите действие--> +,-,*,/");
+                string a = Console.ReadLine();
+                Console.WriteLine("Первое число -->");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Второе число -->");
+                int y = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case "+":
+                        MyDel myDelegat = (c, b) => { return c + b; };
+                        int del = myDelegat(x, y);
+                        Console.WriteLine("Ответ:{0}", del);
+                        break;
+                    case "-":
+                        MyDel myDelegat1 = (c, b) => { return c - b; };
+                        int del1 = myDelegat1(x, y);
+                        Console.WriteLine("Ответ:{0}", del1);
+                        break;
+                    case "*":
+                        MyDel myDelegat2 = (c, b) => { return c * b; };
+                        int del2 = myDelegat2(x, y);
+                        Console.WriteLine("Ответ:{0}", del2);
+                        break;
+                    case "/":
+                        MyDel myDelegat3 = (c, b) => { return c / b; };
+                        int del3 = myDelegat3(x, y);
+                        Console.WriteLine("Ответ:{0}", del3);
+                        break;
+                    default:
+                        Console.WriteLine("Выберите из предложанного");
+                        break;
+                }
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ Program2.3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2c2s.hw1._2
+{
+    internal class Program
+    {
+        delegate int MyDelegate();
+        delegate double MyDel(MyDelegate[] a);
+
+
+        static int GetRandom()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 100);
+            Console.WriteLine(number);
+            return number;
+        }
+
+        static void Main(string[] args)
+
+        {
+            {
+               
+                Console.WriteLine("Ввiдiте число елементiв массиву: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine(new string('-', 50));
+
+                var array = new MyDelegate[n];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = () => new MyDelegate(GetRandom)();
+                }
+
+                MyDel d = delegate (MyDelegate[] a)
+                {
+                    double sr = 0;
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        sr += a[i]();
+                    }
+                    return sr / array.Length;
+                };
+                Console.WriteLine("Середнє арифметичне = {0}", d(array));
+                Console.WriteLine(new string('-', 50));
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]());
+                }
+            }
+            Console.ReadKey();  
+        }
+    }
+}
